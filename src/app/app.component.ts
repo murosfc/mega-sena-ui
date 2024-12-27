93a3d07b9776f82553c69bbb6a53ef868215df60
Felipe Muros <84643909+murosfc@users.noreply.github.com>
2024-12-26 23:42:57 -0300

    chore: ui finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,6 @@ import { Clipboard } from '@angular/cdk/clipboard';
 import { CdkTableModule } from '@angular/cdk/table';
 import { GameService } from './services/game.service';
 import { Game } from '../interfaces/game.interface';
-import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 
@@ -21,34 +20,45 @@ export class AppComponent {
   private readonly snackBar = inject(MatSnackBar);
   readonly games = signal<Game[]>([]);
 
-  title = 'mega-sena-ui';
-  numGames: number = 1;
-  displayedColumns: string[] = ['id', 'name'];
+  readonly title = 'mega-sena-ui';
+  numGames: number | null = null;
+  readonly displayedColumns: string[] = ['id', 'name'];
+  readonly loading = signal<boolean>(false);
+
+  readonly disabled = signal<boolean>(true);
 
   copyToClipboard(text: string): void {
     const successful = this.clipboard.copy(text);
-    
+
     if (successful) {
       this.snackBar.open('Números copiados com sucesso!', 'Fechar', {
         duration: 2000,
         horizontalPosition: 'center',
-        verticalPosition: 'bottom'
+        verticalPosition: 'bottom',
+        panelClass: ['white-snackbar'],
       });
     }
   }
   formatGameNumbers(numbers: number[]): string {
-    return numbers
-      .map(num => num.toString().padStart(2, '0'))
-      .join('-');
+    return numbers.map((num) => num.toString().padStart(2, '0')).join('-');
   }
-  
-  getGames() {    
+
+  getGames() {
+    if (!this.numGames) return;
+    this.games.set([]);
+    this.loading.set(true);
     this.gameService.getGames(this.numGames).subscribe((games) => {
       const gamesWithIds = games.map((game, index) => ({
         ...game,
-        id: index + 1
+        id: index + 1,
       }));
       this.games.set(gamesWithIds);
+      this.loading.set(false);
     });
+  }
+
+  onInputChange(value: number) {
+    this.numGames = value;
+    this.disabled.set(value < 1 || value > 20);
   }
 }
